78e3b3cfd0302e3df690bf0b77eb1b93fe3a0df1
Talal-12 <talal.alothman@gmail.com>
2022-11-14 12:58:34 +0300

    DONE

--- main.dart
+++ main.dart
@@ -1,22 +1,3 @@
-void main() {
-  final movie = {
-    'name': 'The Amazing SpiderMan',
-    'genre': 'Action',
-    'ratings': [1.5, 3.2, 6.4]
-  };
-  print(movie['name']);
-  print(movie['genre']);
-  print(movie['ratings']);
-
-  print("");
-  Movie movie1 = Movie("Black Panther 2", "Action", 4.5);
-  movie1.printDetails();
-
-  print("");
-  Movie movie2 = Movie("Step Brothers", "Comedy", 4.9);
-  movie2.printDetails();
-}
-
 class Movie {
   String? name, genre;
   double? ratings;
@@ -36,4 +17,23 @@ class Movie {
     print('Genre: $genre');
     print('Rating: $ratings');
   }
+}
+
+void main() {
+  final movie = {
+    'name': 'The Amazing SpiderMan',
+    'genre': 'Action',
+    'ratings': [1.5, 3.2, 6.4]
+  };
+  print(movie['name']);
+  print(movie['genre']);
+  print(movie['ratings']);
+
+  print("");
+  Movie movie1 = Movie("Black Panther 2", "Action", 4.5);
+  movie1.printDetails();
+
+  print("");
+  Movie movie2 = Movie("Step Brothers", "Comedy", 4.9);
+  movie2.printDetails();
 }
